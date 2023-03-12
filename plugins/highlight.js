d571884e2d8dc8772aa28e55851860add966a7ca
Sonder <814029073@qq.com>
2023-03-12 12:27:41 +0800

    1

--- plugins/highlight.js
+++ plugins/highlight.js
@@ -11,16 +11,7 @@ Highlight.install = function (Vue) {
     // 被绑定元素插入父节点时调用
     inserted: function(el) {
       let blocks = el.querySelectorAll('pre code');
-      for (let i = 0; i < blocks.length; i++) {
-        hljs.highlightBlock(blocks[i]);
-      }
-    },
-    // 指令所在组件的 VNode 及其子 VNode 全部更新后调用
-    componentUpdated: function(el) {
-      let blocks = el.querySelectorAll('pre code');
-      for (let i = 0; i < blocks.length; i++) {
-        hljs.highlightBlock(blocks[i]);
-      }
+      blocks.forEach(el => { hljs.highlightElement(el) })
     }
   })
 };
