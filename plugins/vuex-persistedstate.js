3fad660de4dc0572ffddda0ba1b77543e6829e47
liuhai <814029073@qq.com>
2023-04-06 10:33:13 +0800

    vuex

--- plugins/vuex-persistedstate.js
+++ plugins/vuex-persistedstate.js
@@ -1,7 +1,8 @@
 import createPersistedState from 'vuex-persistedstate';
-
+const version = '1.0.0'
 export default ({store}) => {
   createPersistedState({
+    key: `vuex_${version}`,
     storage: localStorage,
   })(store)
 }
