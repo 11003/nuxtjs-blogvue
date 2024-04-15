433cc2291e7d9d8af97edd922f4982faec7693ca
刘海 <814029073@qq.com>
2024-04-15 14:11:11 +0800

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,11 @@
 module.exports = {
-    apps: [{
-        name: 'blog-nuxt',
-        script: 'nuxt',
-        args: "start",
-        append_env_to_name: false,
-        cwd: './',
-        exec_interpreter: "bash",
-        exec_mode: "cluster",
-        out_file: "./logs/out-0.log",
-        error_file: "./logs/err-0.log",
-        merge_logs: true,
-        log_data_format: 'YYYY-MM-DD HH:mm Z',
-        autorestart: true,
-        watch: [
-            './nuxt',
-            'nuxt.config.js',
-            'package.json'
-        ],
-        max_memory_restart: '1G',
-        node_args: '--harmony',
-        env: {
-            PORT: 3000,
-            NODE_ENV: "production"
+    apps: [
+        {
+            name: 'nuxt2-blog',
+            exec_mode: 'cluster',
+            instances: '1', // Or a number of instances
+            script: './node_modules/nuxt/bin/nuxt.js',
+            args: 'start'
         }
-    }],
-};
+    ]
+}
